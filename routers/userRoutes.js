8168ad6fdbbe50dd93661faf99f090e09eba889b
Anupa200212 <supulanupa@gmail.com>
2026-05-11 16:08:25 +0530

    connect router ans server

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -6,4 +6,5 @@ router.get("/",(req,res)=>{
     res.send("Server Running")
 })
 
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const userRoutes= require("./routers/userRoutes")
 
 
 app.get("/",(req,res)=>{
     res.send("Server Running")
 });
+
+app.use("/api/users",userRoutes);
 
 app. listen(port,()=>{
     console.log("Server is running on port "+port);

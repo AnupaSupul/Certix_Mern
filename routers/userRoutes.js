7df2268fba53327016c1e7041ae01c9918c02833
Anupa200212 <supulanupa@gmail.com>
2026-05-11 13:32:38 +0530

    all

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const user
+const userRouter = require("./userRoutes")
 
 router.get("/",(req,res)=>{
     res.send("Server Running")

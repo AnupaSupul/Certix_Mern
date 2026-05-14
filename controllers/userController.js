3b9e7404c47aeab52a1e8a57e9c02e6ef86a4bb7
Anupa200212 <supulanupa@gmail.com>
2026-05-14 10:50:11 +0530

    delete crud

--- controllers/userController.js
+++ controllers/userController.js
@@ -42,4 +42,22 @@ const upadateUser = async(req,res)=>{
     }
 }
 
-module.exports={getallUsers,createNewUser,upadateUser}
+const deleteUser = async(req,res)=>{
+    try{
+        const user = await User.findByIdAndDelete(
+            req.params.id
+        ) //Find user by ID and delete from MongoDB
+        res.status(200).json("User deleted successfully")
+
+    }catch(err){
+        res.status(500).json({
+            message : "Failed to delete user",
+            err : err.message
+        })
+    }
+}
+
+module.exports={getallUsers,
+                createNewUser,
+                upadateUser,
+                deleteUser}
--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {getallUsers,createNewUser,upadateUser}= require("../controllers/userController")
+const {getallUsers,createNewUser,upadateUser,deleteUser}= require("../controllers/userController")
 
 router.get("/",getallUsers)
 router.post("/",createNewUser)
 router.put("/:id",upadateUser)
+router.delete("/:id",deleteUser)
+
 
 
 

ae49ae495a60e704709336c9ebc147d430228155
Anupa200212 <supulanupa@gmail.com>
2026-05-18 11:21:28 +0530

    user notfound part created

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,7 +39,16 @@ const deleteUser = async(req,res,next)=>{
         const user = await User.findByIdAndDelete(
             req.params.id
         ) //Find user by ID and delete from MongoDB
-        res.status(200).json("User deleted successfully")
+
+        if(!user){
+            return res.status(404).json({
+                message : "User not found"
+            })
+
+        }
+        res.status(200).json({
+            message : "User deleted successfully"
+        })
 
     }catch(err){
         return next(err)

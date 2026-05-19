cad871657a6764574d0d8cc776f64399f4ae71e2
Anupa200212 <supulanupa@gmail.com>
2026-05-19 09:46:30 +0530

    password comphere and login check

--- controllers/userController.js
+++ controllers/userController.js
@@ -84,10 +84,39 @@ const getSingleUser = async(req,res,next)=>{
     }
 }
 
+const logInUser = async(req,res,next)=>{
+    try{
+        const {email,password} =req.body
+        const user = await User.findOne({email})
 
+        if(!user){
+            return res.status(404).json({
+                message: "User not found"})
+        }
+
+        const isPasswordValid = await bcrypt.compare(
+            password,
+            user.password
+        )
+
+        if(!isPasswordValid){
+            return res.status(401).json({
+                message: "Invalid password"
+            })
+        }else{
+            res.status(200).json({
+                message: "Login successful"
+            })
+        }
+
+    }catch(error){
+        return next(error)
+    }
+}
 
 module.exports={getallUsers,
                 createNewUser,
                 upadateUser,
                 deleteUser,
-                getSingleUser}
+                getSingleUser,
+                logInUser}
--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const {getallUsers,createNewUser,
-    upadateUser,deleteUser, getSingleUser}= require("../controllers/userController")
+    upadateUser,deleteUser,
+     getSingleUser,logInUser}= require("../controllers/userController")
 
 router.get("/",getallUsers)
 router.get("/:id",getSingleUser)
 router.post("/",createNewUser)
 router.put("/:id",upadateUser)
 router.delete("/:id",deleteUser)
+router.post("/login",logInUser)
 
 
 
